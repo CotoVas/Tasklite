6c9876082c856d347deb7f1d97039ed3360d5d98
Василий <gjgsnrf2@gmail.com>
2026-04-19 12:24:33 +0300

    ДЗ+ Kanban (Домашнее задание находится в script.js)

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const now = new Date()
-console.log(now.toLocaleString());
-
-const day = now.getDate().toString().padStart(2, '0')
-const month = (now.getMonth() + 1).toString().padStart(2, '0')
-const year = now.getFullYear()
-
-const hours = now.getHours().toString().padStart(2, '0')
-const minutes = now.getMinutes().toString().padStart(2, '0')
-const seconds = now.getSeconds().toString().padStart(2, '0')
-
-console.log(`${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`);
-
-
-
-// ДЗ 
-
-function getTimeOfDay() {
-    const currentHour = now.getHours();
-    if (currentHour >= 6 && currentHour < 12) {
-        return "Утро";
-    } else if (currentHour >= 12 && currentHour < 18) {
-        return "День";
-    } else if (currentHour >= 18 && currentHour < 23) {
-        return "Вечер";
-    } else {
-        return "Ночь";
-    }
+function normalizeWords(str) {
+    const clean = String(str || "").trim();
+    
+    if (clean === "") return "";
+    
+    const words = clean.split(/\s+/);
+    
+    const normalized = words.map(word => {
+        const lowerWord = word.toLowerCase();
+        return lowerWord[0].toUpperCase() + lowerWord.slice(1);
+    });
+    
+    return normalized.join(" ");
 }
-console.log(getTimeOfDay());
 
+console.log(normalizeWords("  пРиВЕТ  МИР!  "));
+// дз
